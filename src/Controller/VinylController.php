89e4c4e8dcda2522148b43dcb030c4763ac0141d
Silvestre Fernandez <silvestref@missionreadyhq.com>
2024-10-25 15:53:34 +1300

    twig files added

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
-    public function lola(): Response
+    public function homepage(): Response
     {
-        return new Response('Homepage');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Sing for the Moment', 'artist' => 'Eminem'],
+            ['song' => 'Forty Six & Two', 'artist' => 'TOOL'],
+            ['song' =>'Hallowed be thy Name', 'artist' =>  'Iron Maiden'],
+      ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+        'title' => 'Lola',
+        'tracks' => $tracks,
+    ]);
     }
     #[Route('browse/{slug}')]
     public function browse(string $slug = null): Response
